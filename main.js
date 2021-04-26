ee92e9ce68890626f38c5e30627c8ccdbafe437d
ShirishSharan <72022954+ShirishSharan@users.noreply.github.com>
2021-04-26 18:20:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+noseY=0;
+noseX=0;
+
+diffrence=0;
+leftWristX=0;
+rightWristX=0;
+
 function setup(){
  video = createCapture(VIDEO);
  video.size(350,350);
  canvas = createCanvas(400,400);
  canvas.position(500,250);
- pose = ml5.poseNet(video , modelLoaded);
- pose.on('pose' , gotPoses);
+ poseNet = ml5.poseNet(video , modelLoaded);
+ poseNet.on('pose' , gotPoses);
 
 }
  
@@ -15,17 +22,35 @@ function modelLoaded(){
 
 
 
-function draw() {
-    background("black");
-    
-}
+
 
 function gotPoses(results)
 {
-    if(length.results > 0)
+    if(results.length > 0)
     {
         console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX = " + noseX +  "noseY = " + noseY);
+        leftWristX= results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        diffrence= leftWristX - rightWristX;
     }
+
         
     
+}
+
+function draw() {
+    background("white");
+    
+    fill("black")
+    stroke("yellow");
+    text("Namaste🙏🏻" , noseX , noseY , diffrence)
+    //shade("blue");
+    
+    
+    
+    
+
 }
